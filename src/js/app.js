a284be9bbfb771f353ad90227d653dc8cabd947a
Ilya Lovriakov <ilya.lovriakov@booking.com>
2016-01-19 16:21:19 +0100

    adding Firebase to codebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
-console.log('hi')
+import Firebase from 'firebase';
+
+const myRootRef = new Firebase('https://resplendent-inferno-8697.firebaseio.com');
+
+// myRootRef.set("hello world!");
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
     },
     module: {
       loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+        { 
+          test: /\.js$/, 
+          exclude: /node_modules/, 
+          loader: 'babel',
+          query: {
+              presets: ['es2015']
+          }
+        },
         { test: /\.png$/, loader: 'url?limit=100000' },
         { test: /\.jpg$/, loader: 'file' },
         { test: /\.gif$/, loader: 'file' },
